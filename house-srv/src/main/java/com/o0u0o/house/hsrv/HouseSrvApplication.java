cede7680cb15fbac26df2f058da5490ecd549db2
岳川贵 <jobs@techgeng.com>
2022-03-09 17:44:53 +0800

    修复房产业务不启动

--- house-srv/src/main/java/com/o0u0o/house/hsrv/HouseSrvApplication.java
+++ house-srv/src/main/java/com/o0u0o/house/hsrv/HouseSrvApplication.java
@@ -3,7 +3,7 @@ package com.o0u0o.house.hsrv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * <h1>房产服务启动类</h1>
@@ -13,7 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  **/
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableSwagger2
+//@EnableSwagger2
 public class HouseSrvApplication {
 
     public static void main(String[] args) {
--- user-srv/src/main/java/com/o0u0o/house/user/UserServiceApplication.java
+++ user-srv/src/main/java/com/o0u0o/house/user/UserServiceApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
+/**
+ * <h1>用户服务启动类</h1>
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 //@EnableSwagger2
