9037e17900447b0268424f71ec2319260d7cdf0f
岳川贵 <jobs@techgeng.com>
2022-03-09 15:15:39 +0800

    DruidConfig

---
+++ user-srv/src/main/java/com/o0u0o/house/user/config/DruidConfig.java
@@ -0,0 +1,46 @@
+package com.o0u0o.house.user.config;
+
+import com.alibaba.druid.filter.Filter;
+import com.alibaba.druid.filter.stat.StatFilter;
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.google.common.collect.Lists;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+/**
+ * <h1>德鲁伊配置</h1>
+ * @author o0u0o
+ * @date 2022/3/9 3:14 PM
+ */
+@Configuration
+public class DruidConfig {
+
+    @ConfigurationProperties(prefix="spring.druid")
+    @Bean(initMethod="init",destroyMethod="close")
+    public DataSource dataSource(Filter statFilter) throws SQLException {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setProxyFilters(Lists.newArrayList(statFilter()));
+        return dataSource;
+    }
+
+    @Bean
+    public Filter statFilter(){
+        StatFilter filter = new StatFilter();
+        filter.setSlowSqlMillis(5000);
+        filter.setLogSlowSql(true);
+        filter.setMergeSql(true);
+        return filter;
+    }
+
+
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean(){
+        return new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+    }
+}
